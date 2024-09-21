77bcb36ed9938a46e083b116c64e15ca316bb2e1
ankuxsshh <ankuxshh72@gmail.com>
2024-09-21 11:35:48 +0530

    Initial commit

--- highland_project/settings.py
+++ highland_project/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6ruowuu7$q*a)@^b794)x4+si&0&z_kotyu1h7^)-(tj+qb*2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['highland-project.onrender.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
