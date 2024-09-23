edcd642f2950dd0edd7b414ec095043ca8544c7c
ankuxsshh <ankuxshh72@gmail.com>
2024-09-23 12:55:06 +0530

    Initial commit

--- highland_project/settings.py
+++ highland_project/settings.py
@@ -111,13 +111,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Static files settings
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media files settings
 MEDIA_URL = '/media/'
