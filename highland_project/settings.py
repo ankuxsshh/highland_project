4f71e5fa87952b93ffc804c1ff02192932158f88
ankuxsshh <ankuxshh72@gmail.com>
2024-09-23 11:03:27 +0530

    Initial commit

--- highland_project/settings.py
+++ highland_project/settings.py
@@ -117,7 +117,8 @@ USE_TZ = True
 
 # Static files settings
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Add this line for production
 
 # Media files settings
 MEDIA_URL = '/media/'
