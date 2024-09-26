03862a5fad32981ce7815c722075123488fe7400
ankuxsshh <ankuxshh72@gmail.com>
2024-09-26 20:38:55 +0530

    first commit

--- highland_project/settings.py
+++ highland_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6ruowuu7$q*a)@^b794)x4+si&0&z_kotyu1h7^)-(tj+qb*2@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -115,8 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 # Static files settings
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files settings
 MEDIA_URL = '/media/'
